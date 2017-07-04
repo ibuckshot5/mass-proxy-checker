52cde27a1802e656ce0564c93438e254bacac34b
FrostTheFox <thunderfox@thunderfox.nl>
2017-07-04 00:26:55 -0400

    pep8 fixes, add headers

--- proxycheck.py
+++ proxycheck.py
@@ -1,24 +1,29 @@
 import argparse
 import requests
 
+
 def main():
     global args
     args = None
     log('Booting up.')
     log('Mass Proxy Checker by ibuckshot5: v1.0.0')
     parser = argparse.ArgumentParser(description='Check some proxies')
-    parser.add_argument('-pf', '--proxy-file', default='proxies.txt', help=('Proxy file to check.'))
-    parser.add_argument('-bf', '--banned-file', default='banned.txt', help=('File to write banned proxies to (banned)'))
-    parser.add_argument('-gf', '--good-file', default='good.txt', help=('File to write good proxies too (not banned)'))
-    parser.add_argument('-ef', '--error-file', default='error.txt', help=('File to store errored out proxies'))
-    parser.add_argument('-v', '--verbose', default=False, action='store_false', help=('Verbose logging'))
+    parser.add_argument('-pf', '--proxy-file', default='proxies.txt', help='Proxy file to check.')
+    parser.add_argument('-bf', '--banned-file', default='banned.txt', help='File to write banned proxies to (banned)')
+    parser.add_argument('-gf', '--good-file', default='good.txt', help='File to write good proxies too (not banned)')
+    parser.add_argument('-ef', '--error-file', default='error.txt', help='File to store errored out proxies')
+    parser.add_argument('-v', '--verbose', default=False, action='store_false', help='Verbose logging')
     # TODO:
-    # parser.add_argument('-er', '--error-retry', default=False, action='store_false', help=('Retry on proxy error'))
-    # parser.add_argument('-erl', '--error-retry-limit', default=3, help=('Number of times to retry the proxy before it is declared not working'))
+    # parser.add_argument('-er', '--error-retry', default=False, action='store_false', help='Retry on proxy error')
+    # parser.add_argument('-erl', '--error-retry-limit', default=3, help='Number of times to retry the proxy before it is declared not working')
     args = parser.parse_args()
 
-    NIANTIC_URL = 'https://pgorelease.nianticlabs.com/plfe/version'
-    PTC_URL = 'https://sso.pokemon.com/sso/login?locale=en&service=https://www.pokemon.com/us/pokemon-trainer-club/caslogin'
+    niantic_url = 'https://pgorelease.nianticlabs.com/plfe/version'
+    ptc_url = 'https://sso.pokemon.com/sso/login?locale=en&service=https://www.pokemon.com/us/pokemon-trainer-club/caslogin'
+
+    niantic_headers = {'User-Agent': 'Niantic App'}
+    ptc_headers = {'User-Agent': 'pokemongo/1 CFNetwork/811.4.18 Darwin/16.5.0', 'Host': 'sso.pokemon.com'}
+
     # TODO: Make this better, reduce nesting on "with open"
     with open(args.proxy_file, 'r+') as f, open(args.banned_file, 'w+') as banned, open(args.good_file, 'w+') as good, open(args.error_file, 'w+') as error:
         proxies = f.readlines()
@@ -30,9 +35,9 @@ def main():
                 'https': p
             }
             try:
-                verbose_log('Sending HTTP request to {}, watiting for response...'.format(NIANTIC_URL))
-                r = requests.get(NIANTIC_URL, proxies=pr, timeout=5)
-                verbose_log('Received response from {} : {}'.format(NIANTIC_URL, r.status_code))
+                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
+                r = requests.get(niantic_url, proxies=pr, timeout=5, headers=niantic_headers)
+                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
                 if r.status_code == 200:
                     nstatus = '{}, Niantic: 200 OK, proxy is not banned.'.format(p)
                 if r.status_code == 403:
@@ -46,9 +51,9 @@ def main():
             log(nstatus)
 
             try:
-                verbose_log('Sending HTTP request to {}, watiting for response...'.format(NIANTIC_URL))
-                r = requests.get(PTC_URL,proxies=pr, timeout=5)
-                verbose_log('Received response from {} : {}'.format(NIANTIC_URL, r.status_code))
+                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
+                r = requests.get(ptc_url, proxies=pr, timeout=5, headers=ptc_headers)
+                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
                 if r.status_code == 200:
                     pstatus = '{}, PTC: 200 OK, proxy is not banned.'.format(p)
                 if r.status_code == 409:
@@ -78,8 +83,10 @@ def main():
 
     log('Done checking proxies. You can find them at {} for good ones, and {} for banned ones.'.format(args.good_file, args.banned_file))
 
+
 def log(message, char='+'):
     print('[{}] {}'.format(char, message))
+
 
 def verbose_log(message, char='+'):
     if args.verbose:
